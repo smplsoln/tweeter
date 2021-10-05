1894d5e5af6b809800eda583d4d3a5c535b1a3a6
Prashant R <smplsoln@gmail.com>
2021-10-05 01:45:26 -0600

    Nav CSS additions, font additions

--- server/index.js
+++ server/index.js
@@ -4,9 +4,11 @@
 
 const PORT          = 8080;
 const express       = require("express");
+const morgan        = require('morgan');
 const bodyParser    = require("body-parser");
 const app           = express();
 
+app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
