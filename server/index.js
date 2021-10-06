2d1b99a11dd148b62fb9ed7201612ac3afcff003
Prashant R <smplsoln@gmail.com>
2021-10-05 21:09:24 -0600

    Add tweet input form

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@
 
 // Basic express setup:
 
-const PORT          = 3000; //8080;
+const PORT          = 8080;
 const express       = require("express");
 const morgan        = require('morgan');
 const bodyParser    = require("body-parser");
